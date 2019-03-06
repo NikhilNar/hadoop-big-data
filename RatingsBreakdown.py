0604a64f1e41596fc2b591fba8dd9a59e9ab6ee7
Nikhil Nar <ncn251@nyu.edu>
2019-03-06 18:35:32 -0500

    Formatted code

--- RatingsBreakdown.py
+++ RatingsBreakdown.py
@@ -7,12 +7,12 @@ class RatingsBreakdown(MRJob):
             MRStep(mapper=self.mapper_get_ratings, reducer=self.reducer_count_ratings)
         ]
 
-     def mapper_get_ratings(self, _, line):
-         (userID, movieID, rating, timestamp)= line.split('\t')
-         yield rating,1
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp)= line.split('\t')
+        yield rating,1
 
-     def reducer_count_ratings(self, key, values):
-         yield key, sum(values)
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(values)
 
-     if __name__=='__main__':
-         RatingsBreakdown.run()               
+    if __name__=='__main__':
+        RatingsBreakdown.run()               
